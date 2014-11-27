a4ade2e2c56611d0f3a06ea39a2b3d1732c0cf5e
Andrey Lapin <lohmatiy.lapa@undev.ru>
2014-11-27 18:01:24 +0300

    fixed module definition for ie

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ exports.ModelBaseClass = require('./lib/model.js');
 exports.GeoPoint = require('./lib/geo.js').GeoPoint;
 exports.ValidationError = require('./lib/validations.js').ValidationError;
 
-exports.__defineGetter__('version', function () {
+Object.defineProperty(exports, 'version', {
+  get: function() {
     return require('./package.json').version;
+  }
 });
 
-var commonTest = './test/common_test';
-exports.__defineGetter__('test', function () {
+Object.defineProperty(exports, 'test', {
+  get: function() {
     return require(commonTest);
+  }
 });
